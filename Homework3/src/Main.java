47364c7be76ce2708c766ae4c441cae7619edb4b
magicorazon7 <zamolotroka@mail.ru>
2025-10-02 20:41:51 +0700

    max speed methods added

--- Homework3/src/Main.java
+++ Homework3/src/Main.java
@@ -6,29 +6,34 @@ public class Main {
         EV.startEngine();
         EV.startMotion();
         EV.endMotion();
+        EV.getMaxSpeed();
 
         Motorcycle someSuzuki = new Motorcycle("GSX-R100",280);
         someSuzuki.getInfo();
         someSuzuki.startEngine();
         someSuzuki.startMotion();
         someSuzuki.endMotion();
+        someSuzuki.getMaxSpeed();
 
         Ship DemyanBedniy = new Ship("Q-065",30);
         DemyanBedniy.getInfo();
         DemyanBedniy.startEngine();
         DemyanBedniy.startMotion();
         DemyanBedniy.endMotion();
+        DemyanBedniy.getMaxSpeed();
 
         FigherJet suJet = new FigherJet("SU-27M",2400);
         suJet.getInfo();
         suJet.startEngine();
         suJet.startMotion();
         suJet.endMotion();
+        suJet.getMaxSpeed();
 
         GasCar jeep = new GasCar("Land Cruiser Prado",160);
         jeep.getInfo();
         jeep.startEngine();
         jeep.startMotion();
         jeep.endMotion();
+        jeep.getMaxSpeed();
     }
 }
