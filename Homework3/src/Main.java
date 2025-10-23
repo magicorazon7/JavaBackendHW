eb4b6688cad7af5b2393ce4911e78f911c5181a6
magicorazon7 <zamolotroka@mail.ru>
2025-10-23 10:47:13 +0700

    ДЗ по исключениям: В третьем ДЗ был добавлен метод по созданию транспорта с информацией от пользователя. Также добавлены Exception'ы как в первом ДЗ: IllegalArgumentException и InputMismatchException с тем же назначением - предотвращение ввода недействительных аргументов и неподдерживаемых типов данных

--- Homework3/src/Main.java
+++ Homework3/src/Main.java
@@ -1,30 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        GasCar jeep = new GasCar("Land Cruiser Prado",160);
-        jeep.startEngine();
-        jeep.startMotion();
-        jeep.endMotion();
-        jeep.charli();
         
-        VehicleDisplayInfo display = new VehicleDisplayInfo(jeep);
+        VehicleInputScanner newVehicle = new VehicleInputScanner();
+        Vehicle testVehicle = newVehicle.createVehicle();
+    
+        VehicleDisplayInfo display = new VehicleDisplayInfo(testVehicle);
         display.getFuelType();
         display.getMaxSpeed();
         display.getTransportType();
         display.getVehicleName();
-        
-        HybridCar Prius = new HybridCar("Toyota Prius 2010",20000);
-        Prius.startEngine();
-        Prius.startMotion();
-        Prius.endMotion();
-        Prius.legendaryPrius();
-        
-        VehicleDisplayInfo displaySecond = new VehicleDisplayInfo(Prius);
-        displaySecond.getFuelType();
-        displaySecond.getMaxSpeed();
-        displaySecond.getTransportType();
-        displaySecond.getVehicleName();
-        
+    
     }
     
 }
