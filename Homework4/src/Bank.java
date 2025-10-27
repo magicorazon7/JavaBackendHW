e385e392c5ecfadd04a8c9198fc0c80c611b05bf
magicorazon7 <zamolotroka@mail.ru>
2025-10-27 22:22:48 +0700

    Homework6 transfer functions

--- Homework4/src/Bank.java
+++ Homework4/src/Bank.java
@@ -38,4 +38,44 @@ public class Bank {
             o.update(message);
         }
     }
+
+
+    public synchronized void exchangeCurrency(int clientId, Currency from, Currency to, double amount) {
+        Client client = clients.get(clientId);
+        if (client == null) {
+            notifyObservers("Транзакция провалилась: не существует клиента с ID " + clientId);
+            return;
+        }
+        if (!client.getCurrency().equals(from)) {
+            notifyObservers("Транзакция провалилась: клиент " + clientId + " не использует валюту " + from);
+            return;
+        }
+        if (!client.withdraw(amount)) {
+            notifyObservers("Транзакция провалилась: недостаточно средств " + clientId);
+            return;
+        }
+
+        double fromRate = getExchangeRate(from);
+        double toRate = getExchangeRate(to);
+        double converted = amount * (toRate / fromRate);
+        client.deposit(converted);
+
+        //  notifyObservers
+    }
+
+
+    public synchronized void transferFunds(int senderId, int receiverId, double amount) {
+        Client sender = clients.get(senderId);
+        Client receiver = clients.get(receiverId);
+
+        if (sender == null || receiver == null) {
+            notifyObservers("Транзакция правлилась: недействительный ID");
+            return;
+        }
+        if (!sender.withdraw(amount)) {
+            notifyObservers("Транзакция правлилась недостаточно средств у клиента " + senderId);
+            return;
+        }
+
+    }
 }
